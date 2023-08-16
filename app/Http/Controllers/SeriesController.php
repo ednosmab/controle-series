45b009308d24866d455ea4b6ccac0ef099884f4e
Edson Garcia <ednosmab@yahoo.com.br>
2023-08-16 13:55:08 -0300

    alterando idioma em nomes de funções e classes para inglês

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,20 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Serie;
+use App\Models\Series;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = Serie::query()->orderBy('nome')->get();
+        $series = Series::all();
         $mensagemSucesso = $request->session()->get('mensagem.sucesso');
 
         return view('series.index', compact('series', 'mensagemSucesso'));
     }
 
-    public function show(Serie $series)
+    public function show(Series $series)
     {
         return view('series.show')->with('series', $series);
     }
@@ -28,24 +28,24 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Serie::create($request->all());
+        $serie = Series::create($request->all());
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
 
-    public function edit(Serie $series)
+    public function edit(Series $series)
     {
         dd($series->temporadas);
         return view('series.edit')->with('series', $series);
     }
 
-    public function update(Serie $series, SeriesFormRequest $request)
+    public function update(Series $series, SeriesFormRequest $request)
     {
         $series->where('id', '=', $series->id)->update(['nome' => $request->nome]);
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$series->nome}' alterado para '{$request->nome}' com sucesso");
     }
 
-    public function destroy(Serie $series)
+    public function destroy(Series $series)
     {
         $series->delete();
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
