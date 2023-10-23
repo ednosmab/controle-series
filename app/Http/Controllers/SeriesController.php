9f9bc93f1c0fd3daf44a35a8c2e45be6d1193e19
Edson Garcia <ednosmab@yahoo.com.br>
2023-10-23 12:22:09 -0300

    criando classe para o banco, interface para injeção de dependencia com service container, bind para o service provider

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Series;
 use Illuminate\Http\Request;
+use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $repository)
+    {
+        
+    }
+
     public function index(Request $request)
     {
         $series = Series::all();
@@ -33,29 +37,7 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Series::create($request->all());
-        $seasons = [];
-        
-        for ($i = 1; $i <= $request->seasonsQty; $i++) { 
-            $seasons[] = [
-                "series_id" => $serie->id,
-                "number" => $i
-            ];
-        }
-
-        Season::insert($seasons);
-
-        $episodes = [];
-        foreach ($serie->seasons as $season) {
-            for ($j = 1; $j <= $request->episodesPerSeason; $j++) { 
-                $episodes[] = [
-                    "season_id" => $season->id,
-                    "series_id" => $serie->id,
-                    "number" => $i
-                ];
-            }
-        }
-        Episode::insert($episodes);
+        $serie = $this->repository->add($request);
 
         return to_route('series.index')->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
